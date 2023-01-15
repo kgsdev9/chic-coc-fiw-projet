088d05a876fb107593019ee38fbab662e6da8aa3
kgsdev <kgsdev8@gmail.com>
2023-01-15 16:47:46 +0100

    fistcommit

---
+++ app/Models/Product.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable =[
+        'name',
+        'slug',
+        'statut',
+        'descritpion',
+        'price',
+        'img_one',
+        'img_two',
+        'img_three',
+        'popular',
+        'tendance',
+        'quantity',
+        'category_id',
+    ];
+
+    public function category() {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function oders() {
+        return $this->belongsToMany(Order::class,'orders_produtcs','product_id','order_id')
+        ->withPivot('quantity','amount')
+        ->withTimestamps();
+     }
+
+     public function commets() {
+        return $this->hasMany(Comment::class);
+     }
+
+     public function images() {
+        return $this->hasMany(Image::class);
+     }
+}
+
